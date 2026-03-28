85ed81f7db3bd3103d6f76df0a762de7d9163096
조환희 <whghksgmls@gmail.com>
2026-03-28 09:57:01 +0900

    fix package naming in mixins and mod configuration for consistency

--- src/main/java/io/github/hanhy06/emot/config/ConfigListener.java
+++
@@ -1,5 +0,0 @@
-package io.github.hanhy06.emot.config;
-
-public interface ConfigListener {
-	void onConfigReload(Config newConfig);
-}
--- src/main/java/io/github/hanhy06/emot/permission/EmotePermission.java
+++
@@ -1,8 +0,0 @@
-package io.github.hanhy06.emot.permission;
-
-public final class EmotePermission {
-	public static final String DEFAULT_EMOTE_PERMISSION = "emote.use";
-
-	private EmotePermission() {
-	}
-}
