450f98046f5dc17b0547dbf795264b471e3ede70
hanhy06 <whghksgmls@gmail.com>
2026-07-15 14:19:20 +0900

    Combine player skin atlas with baker

--- src/main/java/io/github/hanhy06/emote/skin/PlayerSkinBaker.java
+++ src/main/java/io/github/hanhy06/emote/skin/PlayerSkinBaker.java
@@ -1,16 +1,51 @@
 package io.github.hanhy06.emote.skin;
 
-import io.github.hanhy06.emote.skin.PlayerSkinAtlas.FaceMap;
-import io.github.hanhy06.emote.skin.PlayerSkinAtlas.FaceRect;
-import io.github.hanhy06.emote.skin.PlayerSkinAtlas.PadMode;
-
 import javax.imageio.ImageIO;
 import java.awt.*;
 import java.awt.image.BufferedImage;
 import java.io.ByteArrayOutputStream;
 import java.io.IOException;
+import java.util.Map;
 
 final class PlayerSkinBaker {
+    private static final Map<PlayerSkinPart, PartAtlas> PARTS = Map.of(
+        PlayerSkinPart.HEAD, new PartAtlas(
+            faces(8, 0, 8, 8, 0, 8, 8, 8),
+            faces(40, 0, 8, 8, 32, 8, 8, 8),
+            null,
+            null
+        ),
+        PlayerSkinPart.BODY, new PartAtlas(
+            faces(20, 16, 8, 4, 16, 20, 4, 12),
+            faces(20, 32, 8, 4, 16, 36, 4, 12),
+            null,
+            null
+        ),
+        PlayerSkinPart.RIGHT_ARM, new PartAtlas(
+            faces(44, 16, 4, 4, 40, 20, 4, 12),
+            faces(44, 32, 4, 4, 40, 36, 4, 12),
+            slimRightArmFaces(16, 20),
+            slimRightArmFaces(32, 36)
+        ),
+        PlayerSkinPart.LEFT_ARM, new PartAtlas(
+            faces(36, 48, 4, 4, 32, 52, 4, 12),
+            faces(52, 48, 4, 4, 48, 52, 4, 12),
+            slimLeftArmFaces(false),
+            slimLeftArmFaces(true)
+        ),
+        PlayerSkinPart.RIGHT_LEG, new PartAtlas(
+            faces(4, 16, 4, 4, 0, 20, 4, 12),
+            faces(4, 32, 4, 4, 0, 36, 4, 12),
+            null,
+            null
+        ),
+        PlayerSkinPart.LEFT_LEG, new PartAtlas(
+            faces(20, 48, 4, 4, 16, 52, 4, 12),
+            faces(4, 48, 4, 4, 0, 52, 4, 12),
+            null,
+            null
+        )
+    );
     private static final FaceTarget BASE_TOP = new FaceTarget(8, 0, 8, 8);
     private static final FaceTarget BASE_BOTTOM = new FaceTarget(16, 0, 8, 8);
     private static final FaceTarget BASE_RIGHT = new FaceTarget(0, 8, 8, 8);
@@ -32,8 +67,8 @@ final class PlayerSkinBaker {
         boolean useWideSlimArmAtlas = usesWideSlimArmAtlas(skinPart, effectiveSlimModel);
         BufferedImage bakingImage = useWideSlimArmAtlas ? expandSlimArmToWideAtlas(normalizedImage, skinPart) : normalizedImage;
         BufferedImage outputImage = new BufferedImage(64, 64, BufferedImage.TYPE_INT_ARGB);
-        FaceMap baseFaces = PlayerSkinAtlas.baseFaces(skinPart);
-        FaceMap overlayFaces = PlayerSkinAtlas.overlayFaces(skinPart);
+        FaceMap baseFaces = baseFaces(skinPart);
+        FaceMap overlayFaces = overlayFaces(skinPart);
 
         drawFace(outputImage, bakingImage, baseFaces.top(), BASE_TOP);
         drawFace(outputImage, bakingImage, baseFaces.bottom(), BASE_BOTTOM);
@@ -156,8 +191,8 @@ final class PlayerSkinBaker {
 
     private BufferedImage expandSlimArmToWideAtlas(BufferedImage sourceImage, PlayerSkinPart skinPart) {
         BufferedImage expandedImage = copyImage(sourceImage);
-        copyFaceMap(expandedImage, sourceImage, PlayerSkinAtlas.slimBaseFaces(skinPart), PlayerSkinAtlas.baseFaces(skinPart));
-        copyFaceMap(expandedImage, sourceImage, PlayerSkinAtlas.slimOverlayFaces(skinPart), PlayerSkinAtlas.overlayFaces(skinPart));
+        copyFaceMap(expandedImage, sourceImage, slimBaseFaces(skinPart), baseFaces(skinPart));
+        copyFaceMap(expandedImage, sourceImage, slimOverlayFaces(skinPart), overlayFaces(skinPart));
         return expandedImage;
     }
 
@@ -182,7 +217,102 @@ final class PlayerSkinBaker {
         drawFace(targetImage, sourceImage, sourceRect, new FaceTarget(targetRect.x(), targetRect.y(), targetRect.width(), targetRect.height()));
     }
 
+    private static FaceMap baseFaces(PlayerSkinPart part) {
+        return part(part).base();
+    }
+
+    private static FaceMap overlayFaces(PlayerSkinPart part) {
+        return part(part).overlay();
+    }
+
+    private static FaceMap slimBaseFaces(PlayerSkinPart part) {
+        return requireSlimFaces(part, part(part).slimBase());
+    }
+
+    private static FaceMap slimOverlayFaces(PlayerSkinPart part) {
+        return requireSlimFaces(part, part(part).slimOverlay());
+    }
+
+    private static PartAtlas part(PlayerSkinPart part) {
+        return PARTS.get(part);
+    }
+
+    private static FaceMap requireSlimFaces(PlayerSkinPart part, FaceMap faces) {
+        if (faces == null) {
+            throw new IllegalArgumentException(part + " does not have a slim atlas");
+        }
+        return faces;
+    }
+
+    private static FaceMap faces(
+        int topX,
+        int topY,
+        int topWidth,
+        int topHeight,
+        int rightX,
+        int sideY,
+        int sideWidth,
+        int sideHeight
+    ) {
+        return new FaceMap(
+            new FaceRect(topX, topY, topWidth, topHeight),
+            new FaceRect(topX + topWidth, topY, topWidth, topHeight),
+            new FaceRect(rightX, sideY, sideWidth, sideHeight),
+            new FaceRect(rightX + sideWidth, sideY, topWidth, sideHeight),
+            new FaceRect(rightX + sideWidth + topWidth, sideY, sideWidth, sideHeight),
+            new FaceRect(rightX + sideWidth * 2 + topWidth, sideY, topWidth, sideHeight)
+        );
+    }
+
+    private static FaceMap slimRightArmFaces(int topY, int sideY) {
+        return new FaceMap(
+            slimRect(44, topY, 4, PadMode.LEFT),
+            slimRect(47, topY, 4, PadMode.LEFT),
+            new FaceRect(40, sideY, 4, 12),
+            slimRect(44, sideY, 12, PadMode.LEFT),
+            new FaceRect(47, sideY, 4, 12),
+            slimRect(51, sideY, 12, PadMode.RIGHT)
+        );
+    }
+
+    private static FaceMap slimLeftArmFaces(boolean overlay) {
+        int topY = 48;
+        int sideY = 52;
+        int topX = overlay ? 52 : 36;
+        int rightX = overlay ? 48 : 32;
+        return new FaceMap(
+            slimRect(topX, topY, 4, PadMode.RIGHT),
+            slimRect(topX + 3, topY, 4, PadMode.RIGHT),
+            new FaceRect(rightX, sideY, 4, 12),
+            slimRect(topX, sideY, 12, PadMode.RIGHT),
+            new FaceRect(topX + 3, sideY, 4, 12),
+            slimRect(topX + 7, sideY, 12, PadMode.LEFT)
+        );
+    }
+
+    private static FaceRect slimRect(int x, int y, int height, PadMode padMode) {
+        return new FaceRect(x, y, 3, height, 4, padMode);
+    }
+
     private record FaceTarget(int x, int y, int width, int height) {
+    }
+
+    private record FaceRect(int x, int y, int width, int height, int virtualWidth, PadMode padMode) {
+        private FaceRect(int x, int y, int width, int height) {
+            this(x, y, width, height, width, PadMode.NONE);
+        }
+    }
+
+    private enum PadMode {
+        NONE,
+        LEFT,
+        RIGHT
+    }
+
+    private record FaceMap(FaceRect top, FaceRect bottom, FaceRect right, FaceRect front, FaceRect left, FaceRect back) {
+    }
+
+    private record PartAtlas(FaceMap base, FaceMap overlay, FaceMap slimBase, FaceMap slimOverlay) {
     }
 
 }
