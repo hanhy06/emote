ca09697e8ead6f54a098f03b4e42ea8ac0a93a23
hanhy06 <whghksgmls@gmail.com>
2026-07-15 15:48:55 +0900

    .

--- src/test/java/io/github/hanhy06/emote/emote/PlayableEmoteServiceTest.java
+++ src/test/java/io/github/hanhy06/emote/emote/PlayableEmoteServiceTest.java
@@ -5,7 +5,7 @@ import org.junit.jupiter.api.Test;
 import java.util.List;
 
 import static io.github.hanhy06.emote.test.RegisteredEmoteFixture.create;
-import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 class PlayableEmoteServiceTest {
     @Test
