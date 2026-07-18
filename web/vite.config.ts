1635a257913ce416c0f875b75ee460c03f6d769a
hanhy06 <whghksgmls@gmail.com>
2026-07-18 11:37:50 +0900

    Split web production bundle

--- web/vite.config.ts
+++ web/vite.config.ts
@@ -4,4 +4,24 @@ import react from "@vitejs/plugin-react";
 export default defineConfig(({ command }) => ({
   base: command === "build" ? "/emote/" : "/",
   plugins: [react()],
+  build: {
+    rolldownOptions: {
+      output: {
+        codeSplitting: {
+          groups: [
+            {
+              name: "three",
+              test: /node_modules[\\/]three[\\/]/,
+              priority: 1,
+              maxSize: 400_000,
+            },
+            {
+              name: "vendor",
+              test: /node_modules[\\/]/,
+            },
+          ],
+        },
+      },
+    },
+  },
 }));
