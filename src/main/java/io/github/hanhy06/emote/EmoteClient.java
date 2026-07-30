ae5501b00e45478568723020f3e9302df7e87371
hanhy06 <whghksgmls@gmail.com>
2026-07-30 20:29:19 +0900

    Port key binding to Minecraft 26.3

--- src/main/java/io/github/hanhy06/emote/EmoteClient.java
+++ src/main/java/io/github/hanhy06/emote/EmoteClient.java
@@ -1,5 +1,6 @@
 package io.github.hanhy06.emote;
 
+import com.mojang.blaze3d.platform.InputConstants;
 import io.github.hanhy06.emote.client.EmoteClientNetworking;
 import io.github.hanhy06.emote.client.PerspectiveController;
 import io.github.hanhy06.emote.client.WheelController;
@@ -7,13 +8,12 @@ import net.fabricmc.api.ClientModInitializer;
 import net.fabricmc.fabric.api.client.keymapping.v1.KeyMappingHelper;
 import net.fabricmc.fabric.api.client.networking.v1.ClientPlayConnectionEvents;
 import net.minecraft.client.KeyMapping;
-import org.lwjgl.glfw.GLFW;
 
 public class EmoteClient implements ClientModInitializer {
     private static final PerspectiveController EMOTE_PERSPECTIVE_CONTROLLER = new PerspectiveController();
     private static final WheelController EMOTE_WHEEL_CONTROLLER = new WheelController();
     private static final KeyMapping EMOTE_WHEEL_KEY = KeyMappingHelper.registerKeyMapping(
-        new KeyMapping("key.emote.wheel", GLFW.GLFW_KEY_V, KeyMapping.Category.MISC)
+        new KeyMapping("key.emote.wheel", InputConstants.KEY_V, KeyMapping.Category.MISC)
     );
 
     private final EmoteClientNetworking networking = new EmoteClientNetworking(
