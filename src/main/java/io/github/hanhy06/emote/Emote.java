9dedf03e72e97f5882b1163142313181cce4dbd4
hanhy06 <whghksgmls@gmail.com>
2026-08-14 09:00:52 +0900

    Extract application composition root

--- src/main/java/io/github/hanhy06/emote/Emote.java
+++ src/main/java/io/github/hanhy06/emote/Emote.java
@@ -1,28 +1,6 @@
 package io.github.hanhy06.emote;
 
-import io.github.hanhy06.emote.animation.AnimationDirectoryLoader;
-import io.github.hanhy06.emote.animation.AnimationServerPreparer;
-import io.github.hanhy06.emote.application.ApiEventDispatcher;
-import io.github.hanhy06.emote.application.EmoteApiImpl;
-import io.github.hanhy06.emote.command.AdminCommand;
-import io.github.hanhy06.emote.command.CommandRegistrar;
-import io.github.hanhy06.emote.command.EmoteMenu;
-import io.github.hanhy06.emote.command.UserCommand;
-import io.github.hanhy06.emote.config.ConfigManager;
-import io.github.hanhy06.emote.content.EmoteCatalog;
-import io.github.hanhy06.emote.application.EmotePlayService;
-import io.github.hanhy06.emote.application.EmoteQueryService;
-import io.github.hanhy06.emote.network.PayloadRegistry;
-import io.github.hanhy06.emote.network.PlaybackStateService;
-import io.github.hanhy06.emote.network.WheelSyncService;
-import io.github.hanhy06.emote.permission.PermissionService;
-import io.github.hanhy06.emote.playback.PlaybackEngine;
-import io.github.hanhy06.emote.server.IdlePlaybackService;
-import io.github.hanhy06.emote.server.ReloadService;
-import io.github.hanhy06.emote.server.ServerLifecycle;
-import io.github.hanhy06.emote.skin.PlayerSkinManager;
 import net.fabricmc.api.ModInitializer;
-import net.fabricmc.loader.api.FabricLoader;
 import net.minecraft.server.MinecraftServer;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -34,84 +12,6 @@ public class Emote implements ModInitializer {
 
     @Override
     public void onInitialize() {
-        ConfigManager configManager = new ConfigManager(FabricLoader.getInstance().getConfigDir());
-        EmoteCatalog emoteRegistry = new EmoteCatalog();
-        PermissionService permissionService = new PermissionService();
-
-        PlayerSkinManager playerSkinManager = new PlayerSkinManager();
-        PlaybackEngine playbackEngine = new PlaybackEngine(playerSkinManager);
-        PlaybackStateService playbackStateService = new PlaybackStateService();
-        ApiEventDispatcher apiEvents = new ApiEventDispatcher();
-
-        EmoteQueryService playableEmoteService = new EmoteQueryService(emoteRegistry, permissionService);
-        EmotePlayService playService = new EmotePlayService(
-            emoteRegistry,
-            permissionService,
-            playbackEngine,
-            apiEvents
-        );
-        IdlePlaybackService idlePlaybackService = new IdlePlaybackService(permissionService, playService, playbackEngine);
-
-        EmoteMenu emoteMenu = new EmoteMenu(
-            configManager,
-            emoteRegistry,
-            playableEmoteService,
-            playbackEngine
-        );
-        WheelSyncService wheelSyncService = new WheelSyncService(playableEmoteService);
-
-        new EmoteApiImpl(
-            emoteRegistry,
-            playService,
-            playbackEngine,
-            apiEvents,
-            wheelSyncService::syncAll,
-            new AnimationServerPreparer()
-        );
-        ReloadService reloadService = new ReloadService(
-            configManager,
-            emoteRegistry,
-            new AnimationDirectoryLoader(),
-            playbackEngine,
-            wheelSyncService
-        );
-
-        ServerLifecycle serverLifecycle = new ServerLifecycle(
-            playerSkinManager,
-            emoteRegistry,
-            playbackEngine,
-            reloadService,
-            wheelSyncService,
-            idlePlaybackService
-        );
-        UserCommand userCommand = new UserCommand(
-            playbackEngine,
-            emoteMenu,
-            playableEmoteService,
-            playService
-        );
-        AdminCommand adminCommand = new AdminCommand(
-            emoteRegistry,
-            playbackEngine,
-            permissionService,
-            reloadService,
-            configManager
-        );
-        CommandRegistrar commandRegistrar = new CommandRegistrar(userCommand, adminCommand);
-
-        configManager.addAccessConfigListener(permissionService);
-        configManager.addAccessConfigListener(idlePlaybackService);
-        configManager.addListener(playerSkinManager);
-        configManager.addListener(playbackEngine);
-
-        playbackEngine.addStateListener(playbackStateService);
-        playbackEngine.addStateListener(apiEvents);
-        playbackEngine.registerVisibilityService();
-
-        PayloadRegistry.register();
-        serverLifecycle.register();
-        commandRegistrar.register();
-
-        LOGGER.info("{} ready", MOD_ID);
+        EmoteBootstrap.initialize();
     }
 }
---
+++ src/main/java/io/github/hanhy06/emote/EmoteBootstrap.java
@@ -0,0 +1,77 @@
+package io.github.hanhy06.emote;
+
+import io.github.hanhy06.emote.animation.AnimationDirectoryLoader;
+import io.github.hanhy06.emote.animation.AnimationServerPreparer;
+import io.github.hanhy06.emote.application.ApiEventDispatcher;
+import io.github.hanhy06.emote.application.EmoteApiImpl;
+import io.github.hanhy06.emote.application.EmotePlayService;
+import io.github.hanhy06.emote.application.EmoteQueryService;
+import io.github.hanhy06.emote.command.AdminCommand;
+import io.github.hanhy06.emote.command.CommandRegistrar;
+import io.github.hanhy06.emote.command.EmoteMenu;
+import io.github.hanhy06.emote.command.UserCommand;
+import io.github.hanhy06.emote.config.ConfigManager;
+import io.github.hanhy06.emote.content.EmoteCatalog;
+import io.github.hanhy06.emote.network.PayloadRegistry;
+import io.github.hanhy06.emote.network.PlaybackStateService;
+import io.github.hanhy06.emote.network.WheelSyncService;
+import io.github.hanhy06.emote.permission.PermissionService;
+import io.github.hanhy06.emote.playback.PlaybackEngine;
+import io.github.hanhy06.emote.server.IdlePlaybackService;
+import io.github.hanhy06.emote.server.ReloadService;
+import io.github.hanhy06.emote.server.ServerLifecycle;
+import io.github.hanhy06.emote.skin.PlayerSkinManager;
+import net.fabricmc.loader.api.FabricLoader;
+
+final class EmoteBootstrap {
+    private EmoteBootstrap() {
+    }
+
+    static void initialize() {
+        ConfigManager configManager = new ConfigManager(FabricLoader.getInstance().getConfigDir());
+        EmoteCatalog catalog = new EmoteCatalog();
+        PermissionService permissions = new PermissionService();
+        PlayerSkinManager skins = new PlayerSkinManager();
+        PlaybackEngine playback = new PlaybackEngine(skins);
+        PlaybackStateService playbackState = new PlaybackStateService();
+        ApiEventDispatcher apiEvents = new ApiEventDispatcher();
+        EmoteQueryService queries = new EmoteQueryService(catalog, permissions);
+        EmotePlayService play = new EmotePlayService(catalog, permissions, playback, apiEvents);
+        IdlePlaybackService idlePlayback = new IdlePlaybackService(permissions, play, playback);
+        WheelSyncService wheelSync = new WheelSyncService(queries);
+        ReloadService reload = new ReloadService(
+            configManager,
+            catalog,
+            new AnimationDirectoryLoader(),
+            playback,
+            wheelSync
+        );
+
+        new EmoteApiImpl(
+            catalog,
+            play,
+            playback,
+            apiEvents,
+            wheelSync::syncAll,
+            new AnimationServerPreparer()
+        );
+        CommandRegistrar commands = new CommandRegistrar(
+            new UserCommand(playback, new EmoteMenu(configManager, catalog, queries, playback), queries, play),
+            new AdminCommand(catalog, playback, permissions, reload, configManager)
+        );
+        ServerLifecycle lifecycle = new ServerLifecycle(skins, catalog, playback, reload, wheelSync, idlePlayback);
+
+        configManager.addAccessConfigListener(permissions);
+        configManager.addAccessConfigListener(idlePlayback);
+        configManager.addListener(skins);
+        configManager.addListener(playback);
+        playback.addStateListener(playbackState);
+        playback.addStateListener(apiEvents);
+        playback.registerVisibilityService();
+        PayloadRegistry.register();
+        lifecycle.register();
+        commands.register();
+
+        Emote.LOGGER.info("{} ready", Emote.MOD_ID);
+    }
+}
