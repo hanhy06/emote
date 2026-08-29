6434a338c63cb4accae295f7e9aba29b52e49ffd
hanhy06 <whghksgmls@gmail.com>
2026-08-29 10:40:48 +0900

    Remove BD Engine project importer

--- web/src/import/adapters.ts
+++ web/src/import/adapters.ts
@@ -3,7 +3,6 @@ import type { ImportSource } from "../domain/conversionSeed";
 
 export const IMPORT_ADAPTERS: readonly ImportAdapterLoader[] = [
   lazyAdapter("emote_sequence", "Emote sequence JSON", ["json"], () => import("./emoteJson/sequenceJsonAdapter").then((module) => module.sequenceJsonAdapter)),
-  lazyAdapter("bd_project", "BD Engine project", ["bdengine"], () => import("./bdProject/bdProjectAdapter").then((module) => module.bdProjectAdapter)),
   lazyAdapter("bd_datapack", "BD Engine datapack", ["zip"], () => import("./bdDatapack/bdDatapackAdapter").then((module) => module.bdDatapackAdapter)),
   lazyAdapter("animated_java_json", "Animated Java Plugin Blueprint JSON", ["json"], () => import("./animatedJava/animatedJavaJsonAdapter").then((module) => module.animatedJavaJsonAdapter)),
   lazyAdapter("geckolib_bbmodel", "GeckoLib Blockbench project", ["bbmodel"], () => import("./geckoLibBbmodel/geckoLibBbmodelAdapter").then((module) => module.geckoLibBbmodelAdapter)),
